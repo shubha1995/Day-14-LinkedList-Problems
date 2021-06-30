6fefb5515bafc1290e23340f1d5ba5c046e2c81b
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-30 22:39:02 +0530

    Added: Create Linked List by adding 30 and 56 to 70

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -1,42 +1,49 @@
 package com.ds;
 
 public class LinkedList {
-		Node head;	 
-		
-		class Node{
-			int data;
-			Node next;
-			
-			public Node(int data) {
-				this.data = data;
-//				this.next = null; by default object value is null
-			}
+	Node head;	 
+
+	class Node{
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+		  //this.next = null;   by default object value is null
 		}
-		
-		public Node insert(int data) {
-			Node newNode = new Node(data);
-			if(head == null) {
-				head = newNode;
+	}
+
+	public Node insert(int data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+		}
+		else {
+			Node temp = head;
+			while(temp.next != null) {
+				temp = temp.next;
 			}
-			else {
-				Node temp = head;
-				while(temp.next != null) {
-					temp = temp.next;
-				}
-				temp.next = newNode;
-			}
-			return newNode;
+			temp.next = newNode;
+		}
+		return newNode;
+	}
+
+	public void print() {
+
+		Node temp = head;
+		while(temp != null) {
+			System.out.print(temp.data +  "-> ");
+			temp = temp.next;
 		}
 
-		public void print() {
-			
-				Node temp = head;
-				while(temp != null) {
-					System.out.print(temp.data +  "-> ");
-					temp = temp.next;
-				}
-			
-			}
-			
-		}
+	}
+
+	public void push(int data) {
+		Node newNode = new Node(data);
+		newNode .next = head;
+		head = newNode;
+
+	}
+
+}
 
