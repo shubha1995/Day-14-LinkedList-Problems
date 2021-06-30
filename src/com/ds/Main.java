50bc56527873f8aac8836b4863acb47b3d628564
shubhasankar <ssbhaumikdmr@gmail.com>
2021-06-30 22:19:48 +0530

    Added: Create a simple LinkedList

--- src/com/ds/Main.java
+++ src/com/ds/Main.java
@@ -4,7 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 	
 			System.out.println("Welcome to DataStructure Program");
+
+			LinkedList linkedList = new LinkedList();
+			
+			
+			linkedList.insert(56);
+        	linkedList.insert(30);
+        	linkedList.insert(70);
+
+        	linkedList.print();
         	
 	   }
 	}
-
